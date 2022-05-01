d37203fef5dd1c3174eb1be833038fc8f2a5d84e
DiegoPorta20 <93227425+DiegoPorta20@users.noreply.github.com>
2022-05-01 00:45:59 -0500

    Insert Login, register, home
    Insert Login, register, home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,49 @@
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatIconModule} from "@angular/material/icon";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { LoginComponent } from './components/login/login.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import { RegisterComponent } from './components/register/register.component';
+import { ForgotYourPasswordComponent } from './components/forgot-your-password/forgot-your-password.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
+const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: '', component: LoginComponent },
+  { path: 'forgotYourPassword', component: ForgotYourPasswordComponent},
+  { path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotYourPasswordComponent,
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatDividerModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
